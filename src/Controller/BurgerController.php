14b989ff47b9d049bb351b1d5aef910b46839851
zain.pam1234 <zain.pam1234@gmail.com>
2025-09-12 15:25:22 +0000

    Initialisation et ajouts du controlleur et de la template "Burger"

---
+++ src/Controller/BurgerController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class BurgerController extends AbstractController
+{
+    #[Route('/burgers', name: 'burgers_list')]
+    public function list(): Response
+    {
+        return $this->render('burgers_list.html.twig');
+    }
+}
