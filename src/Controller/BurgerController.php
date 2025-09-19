7265cb809594f3aaf2677c13bd6c850e24f06779
zain.pam1234 <zain.pam1234@gmail.com>
2025-09-19 14:12:16 +0000

    Ajout de quelques exemples de burgers et fin de l'objectif 3

--- src/Controller/BurgerController.php
+++ src/Controller/BurgerController.php
@@ -13,4 +13,22 @@ class BurgerController extends AbstractController
     {
         return $this->render('burgers_list.html.twig');
     }
+
+    #[Route('/burger/{id}', name: 'burger_show')]
+    public function show(int $id): Response
+    {
+        // Simulation d'une liste de burgers
+        $burgers = [
+            1 => ['nom' => 'Burger Classique', 'description' => 'Pain, viande, salade, tomate, sauce.'],
+            2 => ['nom' => 'Burger Végétarien', 'description' => 'Pain, galette végétale, salade, tomate, sauce.'],
+            3 => ['nom' => 'Burger BBQ', 'description' => 'Pain, viande, oignons frits, sauce BBQ.'],
+        ];
+
+        $burger = $burgers[$id] ?? null;
+
+        return $this->render('burger_show.html.twig', [
+            'id' => $id,
+            'burger' => $burger,
+        ]);
+    }
 }
